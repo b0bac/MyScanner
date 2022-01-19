cea5c34c6f9309a533fe268d2cbb80fb0b4d5b5d
b0b@c <1914518025@qq.com>
2022-01-19 19:31:42 +0800

    Update admin.py

--- Work/admin.py
+++ Work/admin.py
@@ -1,9 +1,6 @@
-import os
 from django.contrib import admin, messages
-from django.http import JsonResponse
-
+from .utils import Scan
 from Work.models import Work
-from ExpManager.models import ExpManager
 from django.db import transaction
 
 @admin.register(Work)
@@ -14,19 +11,19 @@ class WorkAdmin(admin.ModelAdmin):
     ordering = ["id"]
     date_hierarchy = 'timestamp'
     @transaction.atomic
-    def scan(self, request, queryset):
-        workid = request.POST.get("_selected_action")
-        taskname = Work.objects.filter(id=workid).values_list("name")[0][0]
-        expid = Work.objects.filter(id=workid).values_list("exp_id")[0][0]
-        vulnid = ExpManager.objects.filter(id=expid).values_list("vulnid")[0][0]
-        ipaddress = Work.objects.filter(id=workid).values_list("ipdst")[0][0]
-        path = ExpManager.objects.filter(id=expid).values_list("fileobj")[0][0]
-        filepath = str(os.getcwd())+"/"+str(path)
-        port = Work.objects.filter(id=workid).values_list("port")[0][0]
-        command_string = ExpManager.objects.filter(id=expid).values_list("command")[0][0]
-        command = command_string%(str(filepath), str(workid), str(taskname), str(vulnid), str(ipaddress), str(port))
-        os.system(command)
-        messages.add_message(request, messages.SUCCESS, '开始扫描%s'%str())
+     def scan(self, request, queryset):
+        workid = None
+        for item in request.POST.lists():
+            if item[0] == "_selected_action":
+                workid = item[1]
+        if isinstance(workid, list):
+            for id in workid:
+                thread = Scan(id)
+                thread.start()
+                messages.add_message(request, messages.SUCCESS, '开始扫描%s' % str(id))
+        else:
+            messages.add_message(request, messages.SUCCESS, '扫描异常')
+
 
     scan.short_description = "启动扫描"
     scan.icon = 'fa fa-rocket'
